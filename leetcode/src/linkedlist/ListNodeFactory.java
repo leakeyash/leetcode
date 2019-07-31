28b6964d981c87a82e18a3d47706e76fb2c923c2
henry li <leakeyash@163.com>
2019-08-01 00:01:14 +0800

    add list node factory

---
+++ leetcode/src/linkedlist/ListNodeFactory.java
@@ -0,0 +1,28 @@
+package linkedlist;
+
+public class ListNodeFactory {
+
+    public static ListNode newListNode(int... values){
+        ListNode head = new ListNode(values[0]);
+        int index = 1;
+        ListNode point = head;
+        while(index < values.length){
+            point.next = new ListNode(values[index]);
+            point = point.next;
+            index ++;
+        }
+        printListNode(head);
+        return head;
+    }
+
+    public static void printListNode(ListNode listNode){
+        while(listNode!=null){
+            System.out.print(listNode.val);
+            listNode = listNode.next;
+            if(listNode != null){
+                System.out.print("->");
+            }
+        }
+
+    }
+}
