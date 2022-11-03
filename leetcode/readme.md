45f88914c9030b8df31c1a3a327cdaae59881f2e
phoebus <leakeyash@163.com>
2022-11-03 23:22:09 +0800

    update score of 316 and 90b

--- leetcode/readme.md
+++ leetcode/readme.md
@@ -18,8 +18,8 @@ My target is 1800 in weekly contests, I suppose the target date is before the en
 | 2022-10-15 | 89 Bi-Weekly | 2 | 1437/3984 | 1611 | +20 |
 | 2022-10-16 | 315 Weekly | 3 | 2384/6490 | 1625 | +14 |
 | 2022-10-23 | 316 Weekly | 2 | 1913/6387 | 1636 | +11 |
-| 2022-10-29 | 90 Bi-Weekly | 3 | 1196/3624 |  |  |
-| 2022-10-30 | 316 Weekly | 3 | 1994/5660 |  |  |
+| 2022-10-29 | 90 Bi-Weekly | 3 | 1196/3624 | 1656 | +20 |
+| 2022-10-30 | 316 Weekly | 3 | 1994/5660 | 1669 | +13 |
 
 ## leetcode links
 
--- leetcode/src/Solution.java
+++ leetcode/src/Solution.java
@@ -6,10 +6,32 @@ import java.util.LinkedList;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println(new Solution().maxRepeating("aaabaaaabaaabaaaabaaaabaaaabaaaaba",
+                "aaaba"));
     }
 
-    public int rob(int[] nums) {
-        return 1;
+    public int maxRepeating(String sequence, String word) {
+        int left = 0;
+        int res = 0;
+        int cur = 0;
+        while(left<=sequence.length()-word.length()) {
+            boolean flag = true;
+            for (int i = left; i < left + word.length(); i++) {
+                if(sequence.charAt(i) != word.charAt(i - left)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                left = left + word.length();
+                cur++;
+            } else {
+                res = Math.max(cur, res);
+                left = left - cur * word.length() + 1;
+                cur = 0;
+            }
+        }
+        return Math.max(cur, res);
     }
     public String mergeAlternately(String word1, String word2) {
         StringBuilder sb = new StringBuilder();
@@ -31,13 +53,14 @@ public class Solution {
         }
         return sb.toString();
     }
+
     public int minCameraCover(TreeNode root) {
         Deque<TreeNode> deque = new LinkedList<>();
         deque.offer(root);
         int level = 1;
         int cnt = 0;
         while (!deque.isEmpty()) {
-            boolean flag = level % 2 == 0 ;
+            boolean flag = level % 2 == 0;
             int size = deque.size();
             for (int i = 0; i < size; i++) {
                 TreeNode node = deque.poll();
